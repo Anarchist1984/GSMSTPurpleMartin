43babc317fb0783cdc7edaa10e26b2c1c19f0dfa
lifeissad <145711984+Anarchist1984@users.noreply.github.com>
2024-04-09 01:34:25 +0000

    Reformatted

---
+++ purplemartin/app/components/Navbar.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import Link from 'next/link';
+
+const Navbar = () => {
+  return (
+    <header className="bg-indigo-500 dark:bg-indigo-900">
+      <nav className="container mx-auto flex justify-between items-center h-16">
+        <Link href="/">
+          <span className="cursor-pointer text-white text-xl font-bold hover:underline hover:animate-pulse">Martin's Roost</span>
+        </Link>
+        <ul className="flex space-x-4 items-center justify-center">
+          <li>
+            <Link href="/projects">
+              <span className="cursor-pointer text-white hover:underline hover:animate-pulse">Projects</span>
+            </Link>
+          </li>
+          <li>
+            <Link href="/blog">
+              <span className="cursor-pointer text-white hover:underline hover:animate-pulse">Blog</span>
+            </Link>
+          </li>
+          <li>
+            <Link href="/about">
+              <span className="cursor-pointer text-white hover:underline hover:animate-pulse">About</span>
+            </Link>
+          </li>
+        </ul>
+        <Link href='/account/signin' className="bg-white text-indigo-500 px-4 py-2 rounded-md hover:bg-indigo-400 hover:text-white">Researcher Dashboard</Link>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
--- purplemartin/app/page.tsx
+++ purplemartin/app/page.tsx
@@ -1,9 +1,11 @@
-import Image from "next/image";
-
+import Navbar from "./components/Navbar";
 export default function Home() {
   return (
-    <main>
-      
+    <main className="">
+      <Navbar />
+      <div className="h-screen bg-indigo-400 dark:bg-indigo-950">
+
+      </div>
     </main>
   );
 }
