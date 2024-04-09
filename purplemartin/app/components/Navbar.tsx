6d3bd8d34e20bf5e49cb51457768567751d0ef9a
S h <145711984+Anarchist1984@users.noreply.github.com>
2024-04-09 22:44:16 +0000

    Created navbar transitions

--- purplemartin/app/components/Navbar.tsx
+++ purplemartin/app/components/Navbar.tsx
@@ -1,47 +1,67 @@
-import React from "react";
+"use client";
+import React, { useState, useEffect } from "react";
 import Link from "next/link";
 import "./components.css";
 
 const Navbar = () => {
+  const [scrollPosition, setScrollPosition] = useState(0);
+  const navbarHeight = 64;
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const position = window.scrollY;
+      setScrollPosition(position);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <header className="bg-indigo-500 dark:bg-indigo-900">
-      <nav className="container mx-auto flex justify-between items-center h-16">
-        <Link href="/">
-          <span className="cursor-pointer text-white text-xl font-bold hover:underline hover:animate-pulse">
-            Martin's Roost
-          </span>
-        </Link>
-        <ul className="flex space-x-4 items-center justify-center">
-          <li>
-            <Link href="/projects">
-              <span className="cursor-pointer text-white hover:underline hover:animate-pulse">
-                Projects
-              </span>
-            </Link>
-          </li>
-          <li>
-            <Link href="/blog">
-              <span className="cursor-pointer text-white hover:underline hover:animate-pulse">
-                Blog
-              </span>
-            </Link>
-          </li>
-          <li>
-            <Link href="/about">
-              <span className="cursor-pointer text-white hover:underline hover:animate-pulse">
-                About
-              </span>
-            </Link>
-          </li>
-        </ul>
-        <Link
-          href="/account/signin"
-          className="bg-white text-indigo-500 px-4 py-2 rounded-md hover:bg-indigo-400 hover:text-white font-bold"
-        >
-          Researcher Dashboard
-        </Link>
-      </nav>
-    </header>
+    <>
+      <header
+        className={`bg-${scrollPosition > 0 ? "white" : "indigo-500"} dark:bg-indigo-900 fixed top-0 w-full z-50 transition-all duration-1000`}
+        style={{ height: `${navbarHeight}px` }}
+      >
+        <nav className="container mx-auto flex justify-between items-center h-full">
+          <Link href="/">
+            <span className="cursor-pointer text-xl font-bold underline-animation">
+              Martin's Roost
+            </span>
+          </Link>
+          <ul className="flex space-x-28 items-center justify-center font-semibold">
+            <li>
+              <Link href="/projects">
+                <span className="cursor-pointer underline-animation">
+                  Projects
+                </span>
+              </Link>
+            </li>
+            <li>
+              <Link href="/blog">
+                <span className="cursor-pointer underline-animation">Blog</span>
+              </Link>
+            </li>
+            <li>
+              <Link href="/about">
+                <span className="cursor-pointer underline-animation">
+                  About
+                </span>
+              </Link>
+            </li>
+          </ul>
+          <Link
+            href="/account/signin"
+            className={`bg-${scrollPosition > 0 ? "indigo-500" : "white"} px-4 py-2 rounded-md hover:bg-indigo-400 hover:text-white font-bold transition duration-300`}
+          >
+            Researcher Dashboard
+          </Link>
+        </nav>
+      </header>
+      <div style={{ paddingTop: `${navbarHeight}px` }}></div>
+    </>
   );
 };
 
--- purplemartin/app/page.tsx
+++ purplemartin/app/page.tsx
@@ -1,9 +1,33 @@
 import Navbar from "./components/Navbar";
+
 export default function Home() {
   return (
-    <main className="dark">
+    <main className="">
       <Navbar />
-      <div className="h-screen bg-indigo-400 dark:bg-indigo-950"></div>
+      <div className="h-screen bg-indigo-600 dark:bg-indigo-950 relative style={{ paddingTop: `${headerHeight}px` }}">
+        <div className="bg-white rounded-lg inline-block px-6 py-10 ">
+          <h1 className="text-5xl font-bold">
+            <span className="bg-clip-text text-transparent bg-gradient-to-r from-purple-500 to-pink-500">
+              The
+            </span>
+          </h1>
+          <h1 className="text-8xl font-bold">
+            <span className="bg-clip-text text-transparent bg-gradient-to-r from-purple-800 to-pink-900">
+              Purple
+            </span>
+          </h1>
+          <h1 className="text-8xl font-bold">
+            <span className="bg-clip-text text-transparent bg-gradient-to-r from-purple-800 to-pink-900">
+              Martin
+            </span>
+          </h1>
+          <h1 className="text-5xl font-bold">
+            <span className="bg-clip-text text-transparent bg-gradient-to-r from-purple-500 to-pink-500">
+              Project
+            </span>
+          </h1>
+        </div>
+      </div>
     </main>
   );
 }
