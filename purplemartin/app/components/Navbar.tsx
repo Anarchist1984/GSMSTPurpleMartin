27d5d5b0a5d10b117e90836e7213edb88477dcf6
S h <145711984+Anarchist1984@users.noreply.github.com>
2024-04-09 14:43:27 +0000

    Added dark Mode

--- purplemartin/app/components/Navbar.tsx
+++ purplemartin/app/components/Navbar.tsx
@@ -1,31 +1,45 @@
-import React from 'react';
-import Link from 'next/link';
+import React from "react";
+import Link from "next/link";
+import "./components.css";
 
 const Navbar = () => {
   return (
     <header className="bg-indigo-500 dark:bg-indigo-900">
       <nav className="container mx-auto flex justify-between items-center h-16">
         <Link href="/">
-          <span className="cursor-pointer text-white text-xl font-bold hover:underline hover:animate-pulse">Martin's Roost</span>
+          <span className="cursor-pointer text-white text-xl font-bold hover:underline hover:animate-pulse">
+            Martin's Roost
+          </span>
         </Link>
         <ul className="flex space-x-4 items-center justify-center">
           <li>
             <Link href="/projects">
-              <span className="cursor-pointer text-white hover:underline hover:animate-pulse">Projects</span>
+              <span className="cursor-pointer text-white hover:underline hover:animate-pulse">
+                Projects
+              </span>
             </Link>
           </li>
           <li>
             <Link href="/blog">
-              <span className="cursor-pointer text-white hover:underline hover:animate-pulse">Blog</span>
+              <span className="cursor-pointer text-white hover:underline hover:animate-pulse">
+                Blog
+              </span>
             </Link>
           </li>
           <li>
             <Link href="/about">
-              <span className="cursor-pointer text-white hover:underline hover:animate-pulse">About</span>
+              <span className="cursor-pointer text-white hover:underline hover:animate-pulse">
+                About
+              </span>
             </Link>
           </li>
         </ul>
-        <Link href='/account/signin' className="bg-white text-indigo-500 px-4 py-2 rounded-md hover:bg-indigo-400 hover:text-white">Researcher Dashboard</Link>
+        <Link
+          href="/account/signin"
+          className="bg-white text-indigo-500 px-4 py-2 rounded-md hover:bg-indigo-400 hover:text-white font-bold"
+        >
+          Researcher Dashboard
+        </Link>
       </nav>
     </header>
   );
--- purplemartin/app/page.tsx
+++ purplemartin/app/page.tsx
@@ -1,11 +1,9 @@
 import Navbar from "./components/Navbar";
 export default function Home() {
   return (
-    <main className="">
+    <main className="dark">
       <Navbar />
-      <div className="h-screen bg-indigo-400 dark:bg-indigo-950">
-
-      </div>
+      <div className="h-screen bg-indigo-400 dark:bg-indigo-950"></div>
     </main>
   );
 }
